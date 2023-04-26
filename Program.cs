65dca36cb2d03e2a8fe46f383dc1b17ce3d319f5
khorneeq <130909083+khorneeq@users.noreply.github.com>
2023-04-26 15:17:48 +0200

    Numer 10

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Numero_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dividable(3));
+            Console.WriteLine(Dividable(14));
+            Console.WriteLine(Dividable(12));
+            Console.WriteLine(Dividable(37));
+            Console.WriteLine(Dividable(2137));
+        }
+        static bool Dividable(int x)
+
+        {
+            return x%3 == 0 || x%7 == 0;
+        }
+
+    }
+}
